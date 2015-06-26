cbecb42be026c08f24947fbc329fc74fd09c623e
roman2013 <romanyu827@gmail.com>
2015-06-26 12:46:09 +0800

    Initial Commit

---
+++ ChildVCPureLayoutTryer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChildVCPureLayoutTryer
+//
+//  Created by Chen YU on 26/6/15.
+//  Copyright (c) 2015 Chen YU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
